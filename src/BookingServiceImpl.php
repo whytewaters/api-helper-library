d61370903a67f93d6bea783b6e7c6a8b3a93100b
Alex Ryder <alex.ryder@whytewaters.com>
2016-12-12 13:24:24 +1300

    update error message for Expose to API

--- src/BookingServiceImpl.php
+++ src/BookingServiceImpl.php
@@ -118,7 +118,7 @@ class BookingServiceImpl implements BookingService {
         $raw_supplier = $this->get_api_client()->api_supplier($supplier_key);
 
         if($raw_supplier == null) {
-            throw new ModelNotFoundException();
+            throw new ModelNotFoundException('Supplier Not Found, check Expose to API setting in RTBS');
         }
 
         return Supplier::fromRaw($raw_supplier);
