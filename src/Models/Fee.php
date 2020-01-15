863453927837f7209a2b1c79a146c3c1e6af2b33
Mark Crook <mark.crook@whytewaters.com>
2020-01-15 15:05:22 +1300

    remove unnecessary fee setters & clarify names

--- src/Models/Fee.php
+++ src/Models/Fee.php
@@ -14,48 +14,24 @@ class Fee {
         return $this->fee_name;
     }
 
-    public function set_fee_name($fee_name) {
-        $this->fee_name = $fee_name;
-    }
-
     public function get_fee_code() {
         return $this->fee_code;
-    }
-
-    public function set_fee_code($fee_code) {
-        $this->fee_code = $fee_code;
     }
 
     public function get_fee_description() {
         return $this->fee_description;
     }
 
-    public function set_fee_description($fee_description) {
-        $this->fee_description = $fee_description;
-    }
-
     public function get_fee_percent_amount() {
         return $this->fee_percent_amount;
-    }
-
-    public function set_fee_percent_amount($fee_percent_amount) {
-        $this->fee_percent_amount = $fee_percent_amount;
     }
 
     public function get_fee_fixed_amount() {
         return $this->fee_fixed_amount;
     }
 
-    public function set_fee_fixed_amount($fee_fixed_amount) {
-        $this->fee_fixed_amount = $fee_fixed_amount;
-    }
-
-    public function get_fee_is_per_itinerary() {
-        return $this->fee_is_per_itinerary;
-    }
-
-    public function set_fee_is_per_itinerary($fee_is_per_itinerary) {
-        $this->fee_is_per_itinerary = $fee_is_per_itinerary;
+    public function is_per_itinerary() {
+        return (bool) $this->fee_is_per_itinerary;
     }
 
     /**
